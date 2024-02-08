dfb5480c07b74191e4d897e4c448c52ed141ea6f
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-02-08 11:11:25 +0200

    Format code for better readability

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 const RPS_CHOICES = ["rock", "paper", "scissors"];
 
 let PLAYING = true,
-  PLAYER_SCORE = 0,
-  COMPUTER_SCORE = 0;
+    PLAYER_SCORE = 0,
+    COMPUTER_SCORE = 0;
 
 let PLAYER_CHOICE, COMPUTER_CHOICE;
 
@@ -29,73 +29,73 @@ const resetButtonElement = document.querySelector(".button--reset");
 // HELPER FUNCTIONS //
 
 const getComputerChoice = function () {
-  const randomNumber = Math.floor(Math.random() * 3);
-  switch (randomNumber) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissors";
-  }
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
 };
 
 const getRoundScore = function (playerSelection, computerSelection) {
-  const indexDifference = RPS_CHOICES.indexOf(playerSelection) - RPS_CHOICES.indexOf(computerSelection);
+    const indexDifference = RPS_CHOICES.indexOf(playerSelection) - RPS_CHOICES.indexOf(computerSelection);
 
-  if (indexDifference !== 0) {
-    if (indexDifference === 1 || indexDifference === -2) {
-      PLAYER_SCORE++;
-      playerScoreElement.textContent = PLAYER_SCORE;
-    } else {
-      COMPUTER_SCORE++;
-      computerScoreElement.textContent = COMPUTER_SCORE;
+    if (indexDifference !== 0) {
+        if (indexDifference === 1 || indexDifference === -2) {
+            PLAYER_SCORE++;
+            playerScoreElement.textContent = PLAYER_SCORE;
+        } else {
+            COMPUTER_SCORE++;
+            computerScoreElement.textContent = COMPUTER_SCORE;
+        }
     }
-  }
 
-  if (PLAYER_SCORE === 5 || COMPUTER_SCORE === 5) {
-    PLAYING = false;
+    if (PLAYER_SCORE === 5 || COMPUTER_SCORE === 5) {
+        PLAYING = false;
 
-    if (PLAYER_SCORE === 5) {
-      playerElement.style.color = "green";
-      computerElement.style.color = "red";
-    } else if (COMPUTER_SCORE === 5) {
-      playerElement.style.color = "red";
-      computerElement.style.color = "green";
+        if (PLAYER_SCORE === 5) {
+            playerElement.style.color = "green";
+            computerElement.style.color = "red";
+        } else if (COMPUTER_SCORE === 5) {
+            playerElement.style.color = "red";
+            computerElement.style.color = "green";
+        }
     }
-  }
 };
 
 // CALLBACK FUNCTIONS //
 
 const playRound = function (event) {
-  event.preventDefault();
-  if (PLAYING) {
-    if (event.target.classList.contains("button")) {
-      PLAYER_CHOICE = event.target.dataset.choice;
-      playerSelectionElement.src = `./images/player-${PLAYER_CHOICE}.png`;
+    event.preventDefault();
+    if (PLAYING) {
+        if (event.target.classList.contains("button")) {
+            PLAYER_CHOICE = event.target.dataset.choice;
+            playerSelectionElement.src = `./images/player-${PLAYER_CHOICE}.png`;
 
-      COMPUTER_CHOICE = getComputerChoice();
-      computerSelectionElement.src = `./images/computer-${COMPUTER_CHOICE}.png`;
+            COMPUTER_CHOICE = getComputerChoice();
+            computerSelectionElement.src = `./images/computer-${COMPUTER_CHOICE}.png`;
+        }
+
+        getRoundScore(PLAYER_CHOICE, COMPUTER_CHOICE);
     }
-
-    getRoundScore(PLAYER_CHOICE, COMPUTER_CHOICE);
-  }
 };
 
 const resetGame = function () {
-  playerElement.style.color = "black";
-  computerElement.style.color = "black";
+    playerElement.style.color = "black";
+    computerElement.style.color = "black";
 
-  PLAYING = true;
+    PLAYING = true;
 
-  PLAYER_SCORE = 0;
-  playerScoreElement.textContent = PLAYER_SCORE;
-  playerSelectionElement.src = `./images/player.png`;
+    PLAYER_SCORE = 0;
+    playerScoreElement.textContent = PLAYER_SCORE;
+    playerSelectionElement.src = `./images/player.png`;
 
-  COMPUTER_SCORE = 0;
-  computerScoreElement.textContent = COMPUTER_SCORE;
-  computerSelectionElement.src = `./images/computer.png`;
+    COMPUTER_SCORE = 0;
+    computerScoreElement.textContent = COMPUTER_SCORE;
+    computerSelectionElement.src = `./images/computer.png`;
 };
 
 // EVENT LISTENERS //
